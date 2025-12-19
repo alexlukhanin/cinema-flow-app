173f9ed1454c9266fa039b219e5dd05e277e73a1
Oleksandr Lukhanin <alex.lukhanin@hotmail.com>
2025-12-19 20:23:33 +0200

    configuration update

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
- 
+
 export default function Init() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome
+      <Text className="text-5xl text-dark-200 font-bold">
+        Welcome!
       </Text>
     </View>
   );
